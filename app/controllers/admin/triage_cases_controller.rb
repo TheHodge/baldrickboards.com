2ef64e3a18fc7c014094b263b8f50210ef31d233
Dom Hodgson <git@hodgetastic.com>
2025-12-16 17:20:24 +0000

    fixes

--- app/controllers/admin/triage_cases_controller.rb
+++ app/controllers/admin/triage_cases_controller.rb
@@ -54,10 +54,21 @@ class Admin::TriageCasesController < Admin::BaseController
     spam_value = params[:spam] == 'true'
     @case.update(spam: spam_value)
     
-    if spam_value
-      redirect_to admin_triage_case_path(@case), notice: 'Case marked as spam and hidden from public views.'
-    else
-      redirect_to admin_triage_case_path(@case), notice: 'Case unmarked as spam and visible in public views.'
+    respond_to do |format|
+      format.html do
+        if spam_value
+          redirect_to admin_triage_case_path(@case), notice: 'Case marked as spam and hidden from public views.'
+        else
+          redirect_to admin_triage_case_path(@case), notice: 'Case unmarked as spam and visible in public views.'
+        end
+      end
+      format.turbo_stream do
+        if spam_value
+          redirect_to admin_triage_case_path(@case, format: :html), notice: 'Case marked as spam and hidden from public views.'
+        else
+          redirect_to admin_triage_case_path(@case, format: :html), notice: 'Case unmarked as spam and visible in public views.'
+        end
+      end
     end
   end
 
